006f3595128415bc0930faa352854a9e64139615
Tycho Charko <t.charko@outlook.com>
2026-06-23 00:30:10 +0200

    🛻 Transfer files correctly

--- build-terra.bat
+++ build-terra.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal
 
-cmake -S . -B build -G "Visual Studio 17 2022" -A x64
+cmake -S . -B bin\build -G "Visual Studio 17 2022" -A x64
 if errorlevel 1 (
     echo Configure failed.
     pause
     exit /b 1
 )
 
-cmake --build build --config Debug
+cmake --build bin\build --config Debug
 if errorlevel 1 (
     echo Build failed.
     pause
